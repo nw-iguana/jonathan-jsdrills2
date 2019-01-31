2d8b1314f26dd407bb7353a6434a0b8a93adfb15
Jonathan Espinosa <jonathan.espinosa@gmail.com>
2019-01-30 20:33:57 -0800

    added daysinmonth function

--- drill2.js
+++ drill2.js
@@ -40,3 +40,35 @@ function decode(string) {
 
     return newStr;
 }
+
+function daysInMonth(month, leapYear) {
+    let febDays = 28;
+    if (leapYear) {
+        febDays = 29;
+    }
+    switch (month) {
+        case 'January':
+        case 'March':
+        case 'May':
+        case 'July':
+        case 'August':
+        case 'October':
+        case 'December':
+            return `${month} has 31 days.`;
+            break;
+
+        case 'February':
+            return `${month} has ${febDays} days.`;
+            break;
+
+        case 'April':
+        case 'June':
+        case 'September':
+        case 'November':
+            return `${month} has 30 days.`;
+            break;
+
+        default:
+            return `Must provide a valid month.`;
+    }
+}
