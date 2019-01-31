daf2a3574b3836c58bdc23a0c3aa91068d56e123
Jonathan Espinosa <jonathan.espinosa@gmail.com>
2019-01-30 20:03:58 -0800

    added beyond function

--- drill2.js
+++ drill2.js
@@ -1,3 +1,21 @@
 let jediName = function(firstName, lastName) {
     return lastName.slice(0, 2) + firstName.slice(0, 1);
+};
+
+function beyond(num) {
+    if (num !== 0) {
+        console.log('And beyond');
+    }
+
+    if (num > 0) {
+        console.log('To infinity');
+    }
+
+    if (num < 0) {
+        console.log('To negative infinity');
+    }
+
+    if (num === 0) {
+        console.log('Staying home');
+    }
 }
