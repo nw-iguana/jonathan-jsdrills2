75a30c6329d5aeb13682fa761cc8844819e378ab
Jonathan Espinosa <jonathan.espinosa@gmail.com>
2019-01-30 20:18:08 -0800

    added decode function

--- drill2.js
+++ drill2.js
@@ -19,3 +19,24 @@ function beyond(num) {
         console.log('Staying home');
     }
 }
+
+function decode(string) {
+    let newArr = string.split(' ');
+    let newStr = '';
+
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i][0] === 'a') {
+            newStr += newArr[i][1];
+        } else if (newArr[i][0] === 'b') {
+            newStr += newArr[i][2];
+        } else if (newArr[i][0] === 'c') {
+            newStr += newArr[i][3];
+        } else if (newArr[i][0] === 'd') {
+            newStr += newArr[i][4];
+        } else {
+            newStr += ' ';
+        }
+    }
+
+    return newStr;
+}
